78543725598394203a48071a29511936ca6d3b55
Piyush patil <098piyushpatil@gmail.com>
2025-02-08 10:59:28 +0530

    add chess game functionality with WebSocket integration

--- backend1/src/GameManager.ts
+++ backend1/src/GameManager.ts
@@ -1,12 +1,18 @@
 import WebSocket from "ws";
+import { INIT_GAME } from "./messages";
+import { Game } from "./Game";
+import { MOVE } from "./messages";
+
 
 export class GameManager {
     private games: Game[];
-    private pendingUsers : WebSocket;
+    private pendingUsers : WebSocket | null;
     private users: WebSocket[];
 
     constructor() {
         this.games = []
+        this.pendingUsers = null
+        this.users = []
     }
 
     addUser(socket: WebSocket){
@@ -21,16 +27,25 @@ export class GameManager {
     private addHandler(socket: WebSocket){
         socket.on("message", (data)=>{
             const message = JSON.parse(data.toString())
-            if(message.type === "create"){
-                // create a new game
-                this.pendingUsers = socket;
-                this.createGame()
-            }
-            if(message.type === "join"){
+            
+            if(message.type === INIT_GAME){
                 // join an existing game
-                this.joinGame(socket)
+                if(this.pendingUsers){
+                    const game = new Game(this.pendingUsers, socket)
+                    this.games.push(game);
+                    this.pendingUsers = null; 
+                } else{
+                    this.pendingUsers = socket
+                }
+                
             }
-
+            
+            if(message.type === MOVE){
+                const game = this.games.find(game => game.player1 === socket || game.player2 === socket)
+                if(game){
+                    game.makeMove(socket , message.move);
+                }
+            }
         })
     }
 
